17b3e80bf0c95326223f8047fa7d763772f1e36a
biasijunior <juniorme7@live.com>
2019-03-20 22:26:24 +0100

    new change in books comparison

--- create_db_pkl.py
+++ create_db_pkl.py
@@ -2,22 +2,23 @@ import time
 import numpy as np
 import glob
 import cv2
-
 import csv
-
 import cPickle as pickle
 
 # Sift and Flann
 sift = cv2.ORB_create()
+sift = cv2.xfeatures2d.SIFT_create()
 bf = cv2.BFMatcher()
 
 # Load all the images
 all_images_to_compare = []
 titles = []
-for f in glob.iglob("./images/books/test/*"):
+
+for f in glob.iglob("./images/train/*"):
     imag = cv2.imread(f)
     image = cv2.cvtColor(imag, cv2.COLOR_BGR2GRAY)
     titles.append(f)
+    print("error")
     all_images_to_compare.append(image)
 
 
@@ -30,6 +31,7 @@ for image_to_compare, title in zip(all_images_to_compare, titles):
 #creating a zipped object of image descriptors with their titles  
 zipped_data = zip(array_des, titles)
 # creating a .pkl database
-output = open('image_dabase.pkl', 'wb')
+print zipped_data
+output = open('./database/book_image_dabase.pkl', 'wb')
 # writing to a database
 pickle.dump(zipped_data, output)
--- test_img_db.py
+++ test_img_db.py
@@ -7,33 +7,34 @@ start_time = time.time()
 
 
 # img = cv2.imread('./images/home/home1.jpg', 0)
-img = cv2.imread('./images/test/original_book.jpg', 0)
-
+img = cv2.imread('./images/testBooks/madam_sampls/5.jpg', 0)
+# print img
 # Initiate STAR detector
-orb = cv2.ORB_create()
+# orb = cv2.ORB_create()
+orb = cv2.xfeatures2d.SIFT_create()
 
 # find the keypoints with ORB
-kp = orb.detect(img, None)
+# kp = orb.detect(img, None)
 
 # compute the descriptors with ORB
-kp, des = orb.compute(img, kp)
-flann = cv2.BFMatcher()
+kp, des = orb.detectAndCompute(img, None)
+bf = cv2.BFMatcher()
 
 # open a database file
-pkl_file = open('image_dabase.pkl', 'rb')
+pkl_file = open('./database/book_image_dabase.pkl', 'rb')
 # get zipped object
 zipped_obj = pickle.load(pkl_file)
 pkl_file.close()
 for image_descriptor, image_name in zipped_obj:
-    matches = flann.knnMatch(des, image_descriptor, k=2)
+    matches = bf.knnMatch(des, image_descriptor, k=2)
   
     good_points = []
     for m, n in matches:
-        if m.distance < 0.6*n.distance:
+        if m.distance < 0.78*n.distance:
             good_points.append(m)
     number_keypoints = 0
     if len(image_descriptor) <= len(des):
-        number_keypoints = len(des)
+        number_keypoints = len(image_descriptor)
     else:
         number_keypoints = len(des)
 
