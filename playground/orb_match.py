c630e1f1d05c31f453bc2c5bc6c34bf77a5a8d8a
biasijunior <juniorme7@live.com>
2019-04-23 11:39:41 +0100

    getting distance from first match

--- playground/orb_match.py
+++ playground/orb_match.py
@@ -1,10 +1,14 @@
 import numpy as np
 import cv2 as cv
 import matplotlib.pyplot as plt
-img1 = cv.imread('../images/testBooks/arabic_samples/5.jpg', 0)          # queryImage
-img2 = cv.imread('../images/train/arabic.jpg', 0)  # trainImage
+
+
+img1 = cv.imread('../images/train/arabic.jpg', 0)          # queryImage
+img2 = cv.imread('../images/testBooks/arabic/arabic_90.jpg', 0)  # trainImage
 # Initiate ORB detector
 orb = cv.xfeatures2d.SIFT_create()
+# orb = cv.ORB_create()
+
 # find the keypoints and descriptors with ORB
 kp1, des1 = orb.detectAndCompute(img1, None)
 kp2, des2 = orb.detectAndCompute(img2, None)
@@ -15,10 +19,31 @@ bf = cv.BFMatcher()
 
 # Match descriptors.
 matches = bf.match(des1, des2)
-print matches.shape
+# print matches.shape
 # Sort them in the order of their distance.
+
+
+print len(des2)
+print "sorting matches..."
 matches = sorted(matches, key=lambda x: x.distance)
 # Draw first 10 matches.
 
-img3 = cv.drawMatches(img1, kp1, img2, kp2,matches[:100],None, flags=2)
-plt.imshow(img3), plt.show()
+# print matches.distance[10]
+# p1 = matches[0].distance
+# print p1
+
+similarity = (float(len(matches))/ max(len(des1),len(des2)))* 100
+
+print (str(similarity) + "%")
+# for m in matches:
+#     print m.distance
+
+print "//////////-------///////////////----////////////////-----///////////"
+for i in range(0,10):
+    p1 = matches[i].distance
+    print('%.5f' % p1)
+    # print p1
+# print(p1)
+# print('%.5f' % p1)
+# img3 = cv.drawMatches(img1, kp1, img2, kp2,matches[:100],None, flags=2)
+# plt.imshow(img3), plt.show()
