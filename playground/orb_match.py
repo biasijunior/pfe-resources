b93043177833aab205d9f1c209ee48020b74f2c6
ikramAchar <ikram.achar@yahoo.fr>
2019-04-23 11:49:39 +0100

    new commits xD

--- playground/orb_match.py
+++ playground/orb_match.py
@@ -1,10 +1,13 @@
 import numpy as np
 import cv2 as cv
 import matplotlib.pyplot as plt
-img1 = cv.imread('../images/testBooks/arabic_samples/5.jpg', 0)          # queryImage
+img1 = cv.imread('../images/testBooks/arabic/arabic_cropped.jpg', 0)          # queryImage
 img2 = cv.imread('../images/train/arabic.jpg', 0)  # trainImage
 # Initiate ORB detector
-orb = cv.xfeatures2d.SIFT_create()
+# orb = cv.xfeatures2d.SURF_create()
+# orb = cv.xfeatures2d.SIFT_create()
+# orb = cv.ORB_create()
+orb = cv.AKAZE_create()
 # find the keypoints and descriptors with ORB
 kp1, des1 = orb.detectAndCompute(img1, None)
 kp2, des2 = orb.detectAndCompute(img2, None)
@@ -15,10 +18,16 @@ bf = cv.BFMatcher()
 
 # Match descriptors.
 matches = bf.match(des1, des2)
-print matches.shape
+
+# print matches.shape
 # Sort them in the order of their distance.
+
 matches = sorted(matches, key=lambda x: x.distance)
+
+for i in range(0,10):
+    # print matches[i].trainIdx
+    print matches[i].distance
 # Draw first 10 matches.
 
-img3 = cv.drawMatches(img1, kp1, img2, kp2,matches[:100],None, flags=2)
-plt.imshow(img3), plt.show()
+# img3 = cv.drawMatches(img1, kp1, img2, kp2,matches[:100],None, flags=2)
+# plt.imshow(img3), plt.show()
