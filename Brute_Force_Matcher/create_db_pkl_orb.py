ce672aa5fbf9700cb73ac5783c263f44250b25f8
biasijunior <juniorme7@live.com>
2019-06-22 12:19:55 +0100

    descriptor

--- Brute_Force_Matcher/create_db_pkl_orb.py
+++ Brute_Force_Matcher/create_db_pkl_orb.py
@@ -8,7 +8,7 @@ import sys
 # import _pickle as pickle
 # Sift and Flann
 print("satrt")
-sift = cv2.ORB_create()
+sift = cv2.AKAZE_create()
 bf = cv2.BFMatcher()
 
 # Load all the images
@@ -18,11 +18,12 @@ i = 1
 
 print("loading images \r")
 
-for f in glob.iglob("../images/train/*"):
+for f in glob.iglob("../Work/original_images/*"):
     imag = cv2.imread(f)
     image = cv2.cvtColor(imag, cv2.COLOR_BGR2GRAY)
     titles.append(f.rsplit('/', 1)[1])
-    print (i, sep=' ', end='', flush=True)
+#     print (i,sep=' ', end='', flush=True)
+    print (i,'$', sep=' ', end='', flush=True)
     sys.stdout.flush()
     all_images_to_compare.append(image)
     i += 1
@@ -30,9 +31,11 @@ for f in glob.iglob("../images/train/*"):
 array_des = []
 j = 1
 print('computing descriptors in percentage(%) ...')
+image_dict = {}
 for image_to_compare, title in zip(all_images_to_compare, titles):
         # get key points and descriptors
         kp_2, desc_2 = sift.detectAndCompute(image_to_compare, None)
+        image_dict[title] = desc_2
         # creating an array of decriptors
         array_des.append(desc_2)
         remaing = j/float(len(titles)) * 100
@@ -44,6 +47,6 @@ zipped_data = zip(array_des, titles)
 # creating a .pkl database
 
 print('% \n saving to database...')
-output = open('../database/orb_descriptors.pkl', 'wb')
+output = open('../database/akaze_descriptors_dic.pkl', 'wb')
 # writing to a database
-pickle.dump(zipped_data, output)
+pickle.dump(image_dict, output)
--- Brute_Force_Matcher/create_db_pkl_sift.py
+++ Brute_Force_Matcher/create_db_pkl_sift.py
@@ -12,17 +12,18 @@ sift = cv2.xfeatures2d.SIFT_create()
 bf = cv2.BFMatcher()
 
 # Load all the images
-all_images_to_compare = []
+all_images_to_compare = []                              
 titles = []
 i = 1
 
 print("loading images \r")
 
-for f in glob.iglob("../images/train/*"):
+for f in glob.iglob("../Work/original_images/*"):
     imag = cv2.imread(f)
     image = cv2.cvtColor(imag, cv2.COLOR_BGR2GRAY)
     titles.append(f.rsplit('/', 1)[1])
-    print (i, sep=' ', end='', flush=True)
+#     print (i,sep=' ', end='', flush=True)
+    print (i,'$', sep=' ', end='', flush=True)
     sys.stdout.flush()
     all_images_to_compare.append(image)
     i += 1
@@ -30,9 +31,11 @@ for f in glob.iglob("../images/train/*"):
 array_des = []
 j = 1
 print('computing descriptors in percentage(%) ...')
+image_dict = {}
 for image_to_compare, title in zip(all_images_to_compare, titles):
         # get key points and descriptors
         kp_2, desc_2 = sift.detectAndCompute(image_to_compare, None)
+        image_dict[title] = desc_2
         # creating an array of decriptors
         array_des.append(desc_2)
         remaing = j/float(len(titles)) * 100
@@ -42,8 +45,8 @@ for image_to_compare, title in zip(all_images_to_compare, titles):
 #creating a zipped object of image descriptors with their titles  
 zipped_data = zip(array_des, titles)
 # creating a .pkl database
-# print zipped_data
+
 print('% \n saving to database...')
-output = open('../database/book_image_dabase_many_sift.pkl', 'wb')
+output = open('../database/sift_descriptors_dic.pkl', 'wb')
 # writing to a database
-pickle.dump(zipped_data, output)
+pickle.dump(image_dict, output)
